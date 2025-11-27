ca616f131fe9ac3fc10e2da3acc9ccdf7619c118
EmileLL <elarochellelangevin@gmail.com>
2025-11-26 19:07:51 -0500

    implementation schema(movie)

--- src/api/shemas/shema_movie.py
+++ src/api/shemas/shema_movie.py
@@ -1,1 +1,13 @@
 from pydantic import BaseModel
+
+class movieCreate(BaseModel):
+    title : str
+
+class movieResponce(BaseModel):
+    title : str
+    category : str
+    description : str
+    release_date : str
+    rating : str
+    poster_url : str
+    updated_at : str
