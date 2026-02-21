9d914a931424c5f1b759de6ac0522be8fe853ae4
LordEmile <elarochellelangevin@gmail.com>
2026-02-20 21:10:01 -0500

    feat(media-service): add RabbitMQ setup and connection handling

--- services/media-service/main.py
+++ services/media-service/main.py
@@ -1,9 +1,39 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+import os
 
-app = FastAPI()
+from config.messaging.connect import ConnectRabbitMQ
+from config.messaging.manage import ManageRabbitMQ
+from config.messaging.receive import EventReceiver
+from config.messaging.send import EventPublisher
 
 
+RABBITMQ_URL = os.getenv("RABBITMQ_URL")
+connection = ConnectRabbitMQ(url=RABBITMQ_URL)
+manage = ManageRabbitMQ(connection=connection)
+receiveComplete = EventReceiver(manager=manage)
+receive = EventReceiver(manager=manage)
+send = EventPublisher(manager=manage)
+
+async def tmp(payload):
+    print("Media-service: job done")
+
+@asynccontextmanager
+async def startup(app: FastAPI):
+    await connection.connect()
+    await manage.setup()
+    await receive.start(
+        queue_name="media.service",
+        routing_key="pipeline.step.completed",
+        handler=tmp
+    )
+    print("Connexion with rabbitMQ establish")
+    yield
+    print("Connexion with rabbitMQ closed")
+
+
+app = FastAPI(lifespan=startup)
 
 @app.get("/health")
-def chek_health():
+async def chek_health():
     return{"status":"ok"}
